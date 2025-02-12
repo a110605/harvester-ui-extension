cd6e3cae60fd1b84a7d8906348caaef2f3b6fe82
andy.lee <andy.lee@suse.com>
2025-02-12 23:06:11 +0800

    add debugger
    Signed-off-by: andy.lee <andy.lee@suse.com>

--- pkg/harvester/models/harvester/schema.js
+++ pkg/harvester/models/harvester/schema.js
@@ -3,4 +3,12 @@ import SteveSchema from '@shell/models/steve-schema';
 /**
  * This is the steve schema that's used for the harvester store
  */
-export default class HarvesterStoreSchema extends SteveSchema { }
+export default class HarvesterStoreSchema extends SteveSchema {
+  constructor(...args) {
+    console.log('HarvesterStoreSchema constructor args:', args);
+    if (args[0].type === 'type of resource that throws error') {
+      debugger;
+    }
+    super(...args);
+  }
+ }
